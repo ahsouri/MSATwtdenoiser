6a6dbc6115a2ff31901efc5904fd481e2982ebd5
Amir Souri <ahsouri@cfa.harvard.edu>
2021-07-18 11:13:10 -0400

    modify setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup
 setup(name='MSATwtdenoiser',
       version='0.0.1',
       description='Denoising a 2D signal based on wavelet',
+      long_description='',
       url='',
       author='Amir Souri',
       author_email='ahsouri@gmail.com',
       license='MIT',
-      packages = ["MSATwtdenoiser"],
+      packages = ["main"],
       install_requires=[
           'numpy','matplotlib','scipy','pywt'
       ],
